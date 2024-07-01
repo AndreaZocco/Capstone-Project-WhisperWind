defa638bd8f77ba6b8d6a9b6e9e99dd5b7557be8
AndreaZocco <a_zocco@yahoo.it>
2024-07-01 22:18:52 +0200

    adding netlify.toml

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     presets: ["@babel/preset-env", "@babel/preset-react"],
-    plugins: ["@babel/plugin-transform-private-property-in-object"]
+    plugins: ["@babel/plugin-transform-private-property-in-object"] //dd
   };
   
